d969af091abbbdc8dd7c3bf174903f872439367d
Anthony Corrado <amcstudios@yahoo.com>
2014-05-17 18:24:25 -0700

    styling improvement for show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,12 @@
 Bar.destroy_all
 
 bars = Bar.create([
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
-{name: 'JPs Sports Bar and Grill', day: 'tuesday', time: '9:00pm', theme: 'general', web: 'http://www.yelp.com/biz/jps-sports-bar-and-grill-santa-monica#query:bar%20trivia', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
+{name: '', day: 'tuesday', time: '9:00pm', theme: 'general', web: '', lat: '34.024743', lon: '-118.491736'},
 ])
